2bf8a470999fc655f8ddf0935025f8c995359a0a
Tuelomaldito <tiagoreiscruz14@gmail.com>
2024-09-19 21:23:58 -0300

    aula19-09-24

--- src/main/java/com/fatec/controle_financeiro/controllers/ClienteController.java
+++ src/main/java/com/fatec/controle_financeiro/controllers/ClienteController.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
  
-//http://localhost:8090/api/clientes
+// http://localhost:8090/api/clientes
+// caminho para abrir o codigo,tanto na Browser ou HOOPSCOTCH
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -15,6 +16,7 @@ public class ClienteController {
     private int nextId = 1;
  
     // POST - LANÇAR
+    // Responsavel por criar o clinete no HOOPSCOTCH, pela estrutura do codigo ele cria o ID automatico somando eles
     @PostMapping
     public Cliente createCliente(@RequestBody Cliente cliente) {
         cliente.setId(nextId++);
@@ -23,21 +25,15 @@ public class ClienteController {
     }
     // http://localhost:8090/api/clientes
     // GET - LISTAR
+    // Responsavel por listar o todos os cliente criados no POST
     @GetMapping
     public List<Cliente> getAllClientes() {
         return clientes;
     }
 
-    // 
-    @GetMapping("/{id}")
-    public Cliente getClienteById(@PathVariable int id) {
-        Optional<Cliente> cliente = clientes.stream()
-                .filter(c -> c.getId() == id)
-                .findFirst();
-        return cliente.orElse(null);
-    }
     // http://localhost:8090/api/clientes/id
     // PUT - ALTERAR
+    // Responsavel por alterar o nome do cliente com base no ID dele
     @PutMapping("/{id}")
     public Cliente updateCliente(@PathVariable int id, @RequestBody Cliente clienteAtualizado) {
         for (Cliente cliente : clientes) {
@@ -50,6 +46,7 @@ public class ClienteController {
     }
     // http://localhost:8090/api/clientes/id
     // DELETE - APGAR
+    // Responsavel por apagar o cliente com base no ID dele 
     @DeleteMapping("/{id}")
     public String deleteCliente(@PathVariable int id) {
         clientes.removeIf(cliente -> cliente.getId() == id);
